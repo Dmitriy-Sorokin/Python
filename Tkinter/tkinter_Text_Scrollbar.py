1848a33c1b858dcb785ea19d92d8774d6e12adbb
Dmitriy Sorokin <sorokin.da92@gmail.com>
2022-07-18 15:12:35 +0300

    scrollbar

--- Tkinter/tkinter_Text_Scrollbar.py
+++ Tkinter/tkinter_Text_Scrollbar.py
@@ -19,6 +19,22 @@ t = Text(f_text, bg='#343D46', fg='#C6DEC1', padx=10, pady=10, wrap=WORD, insert
 t.pack(fill=BOTH, expand=1, side=LEFT)
 
 
+def add_str():
+    t.insert('2.4', 'Hello!')
+
+def del_str():
+    t.delete('2.4', '2.10')
+    # t.delete('1.0', END)
+
+def get_str():
+    print(t.get('1.0', END))
+
+btn_add = Button(root, text='Add', command=add_str).place(x=50, y=10)
+btn_del = Button(root, text='Delete', command=del_str).place(x=90, y=10)
+btn_get = Button(root, text='Get', command=get_str).place(x=140, y=10)
+
+
+
 scroll = Scrollbar(f_text, command=t.yview)
 scroll.pack(fill=Y, side=LEFT)
 t.config(yscrollcommand=scroll.set)
