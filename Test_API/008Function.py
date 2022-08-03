0486903d834124349d9a96cfb43a4a14b71f99f8
Dmitriy Sorokin <sorokin.da92@gmail.com>
2022-08-03 15:48:07 +0300

    api

---
+++ Test_API/008Function.py
@@ -0,0 +1,30 @@
+# num_1 = 10
+# num_2 = 20
+#
+# result = num_1 + num_2
+# print(result)
+#
+#
+# num_1 = 30
+# num_2 = 40
+#
+# result = num_1 + num_2
+# print(result)
+
+
+# def summ(num_1, num_2):
+#     result = num_1 + num_2
+#     print(result)
+#
+# summ(num_2="world", num_1="Hello ")
+
+name = "Alex"
+age = "20"
+
+def hi(name, age):
+    result = name + " " + age
+    return result
+
+h = hi(name, age)
+
+print(h)
